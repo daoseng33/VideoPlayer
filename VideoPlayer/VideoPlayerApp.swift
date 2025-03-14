a222ac747b940f4ab30f974f93d2295ef29577c8
DAO <daoseng33@gmail.com>
2025-03-14 16:09:54 +0800

    Initial commit

---
+++ VideoPlayer/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  VideoPlayer
+//
+//  Created by DAO on 2025/3/14.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VideoPlayerTests/VideoPlayerTests.swift
@@ -0,0 +1,16 @@
+//
+//  VideoPlayerTests.swift
+//  VideoPlayerTests
+//
+//  Created by DAO on 2025/3/14.
+//
+
+import Testing
+
+struct VideoPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
